0a630a96a32eb1f1dc0ced72d63f3e547c9d48a9
wangxuzhong <1044577650@qq.com>
2020-07-02 18:03:06 +0800

    image组件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
+    pUrl:''
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //获取相册的图片地址
+  getUrl(){
+    //系统api获取相册图片或者拍照
+    wx.chooseImage({
+     success:(res) =>{
+       console.log (res)
+       this.setData({
+        pUrl:res.tempFilePaths[0]
+       })
+     }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  pic(events){
+    console.log (events)//获取图片的相关信息
   }
 })
