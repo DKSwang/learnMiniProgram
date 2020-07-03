e253b6e455c0a634750e256d50bc143a988c9828
wangxuzhong <1044577650@qq.com>
2020-07-03 13:46:16 +0800

    input组件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
+    newVal:'input的绑定数据'
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //获取绑定值
+  getInputVal(val){
+    console.log(val,"监控输入的值,只要输入就会触发")
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取焦点
+  oneHandler(){
+    console.log('获取焦点')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //失去焦点
+  twoHandler(){
+    console.log('失去焦点')
   }
 })
