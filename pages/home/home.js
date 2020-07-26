0bf4150ad2bb4f628fd5663a4e90958444cb4bd0
wangxuzhong <1044577650@qq.com>
2020-07-26 17:24:35 +0800

    07事件冒泡和捕获

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
+    title:['衣服','鞋子','帽子']
 
   },
-
+  //点击事件
+  itemHandler(event){
+    console.log(event,"点击事件")
+    //想要在点击有衣服获取到衣服所对应的下表和拿到衣服这个值--通过data-传递参数
+    console.log(event.target.dataset)
+  },
+//事件冒泡和时间捕获 capture-bind:tap="viewCapture1" bindtap="clickView1"
+viewCapture1(){
+  console.log("view1")
+},
+clickView1(){
+  console.log("点击view1")
+},
+viewCapture2(){
+  console.log("view2")
+},
+clickView2(){
+  console.log("点击view2")
+},
+viewCapture3(){
+  console.log("view3")
+},
+clickView3(){
+  console.log("点击view3")
+},
   /**
    * 生命周期函数--监听页面加载
    */
