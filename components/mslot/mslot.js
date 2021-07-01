c08a2da06f3dba974144d4ed75313dfed43acbae
wangxuzhong <1044577650@qq.com>
2021-07-01 14:52:55 +0800

    插槽的使用-单个插槽和火哥插槽

--- components/mslot/mslot.js
+++ components/mslot/mslot.js
@@ -1,5 +1,8 @@
-// components/changeInside/changeInside.js
+// components/mslot/mslot.js
 Component({
+  options:{
+    multipleSlots:true 
+  },
   /**
    * 组件的属性列表
    */
@@ -11,7 +14,6 @@ Component({
    * 组件的初始数据
    */
   data: {
-    num:0
 
   },
 
@@ -19,11 +21,6 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    myNum(params){
-      this.setData({
-        num:this.data.num+params
-      })
-    }
 
   }
 })
